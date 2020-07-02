1c447c8d9846d7aa3f2a887a84d5312a639381ed
M-Hammad0 <hammad_205@hotmail.com>
2020-07-02 15:24:45 +0500

    add line graph

--- src/api/index.js
+++ src/api/index.js
@@ -16,3 +16,14 @@ export const fetchData = async () => {
     return info;
   } catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    return data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+  } catch (error) {}
+};
